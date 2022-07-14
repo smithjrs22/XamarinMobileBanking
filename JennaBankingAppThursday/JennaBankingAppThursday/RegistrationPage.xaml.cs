6fd265d0744bbf0168cb288be4182f88976cdcfd
9CD1KC2\admin <smithjrs22@gmail.com>
2022-07-14 12:43:07 -0400

    video one finished and working

---
+++ JennaBankingAppThursday/JennaBankingAppThursday/RegistrationPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using JennaBankingAppThursday.Tables;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JennaBankingAppThursday
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+        //when clicked handles our sqlite database?
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //this creates the database
+            var dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDatabase.db");
+            var db = new SQLiteConnection(dbpath); //collects sql path
+            //create table in database
+            db.CreateTable<RegUserTable>();
+
+            var item = new RegUserTable()
+            {
+                UserName = EntryUserName.Text,
+                Password = EntryUserPassword.Text,
+                Email = EntryUserEmail.Text,
+                PhoneNumber = EntryUserPhoneNumber.Text
+            };
+            //insert table inside database
+            db.Insert(item);
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Congratulations", "User Registration successfull", "Yes", "Cancel");
+
+            });
+
+
+        }
+
+        //private void Button_Clicked(object sender, EventArgs e){}
+    }
+}
